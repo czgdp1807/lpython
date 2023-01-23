7780a933eff06f5df3ecf7367927a71f029d1d0b
Gagandeep Singh <gdp.1807@gmail.com>
2023-01-23 12:02:07 +0530

    Benchmark ready for nbody problem in LPython

--- integration_tests/nbody.py
+++ integration_tests/nbody.py
@@ -5,17 +5,21 @@ from math import sqrt
 n: i32
 n = TypeVar("n")
 
+# TODO: Allow returning f32[:, :] from functions
 def lpython_nbody(positions: f32[:, :], weights: f32[:], n: i32) -> f32[n, 2]:
+    i: i32; j: i32
     # TODO: replace the following loop with np.zeros_like
     # accelerations = zeros_like(positions)
     accelerations: f32[n, 2] = empty((n, 2), dtype=float32)
+    for i in range(n):
+        accelerations[i, 0] = f32(0.0)
+        accelerations[i, 1] = f32(0.0)
     # TODO: support getting n from .shape attribue of array.
     # For now it is passed as an argument
     # n = weights.shape[0]
     ax: f32; ay: f32; rx: f32; ry: f32;
     sqr_dist: f32; sixth_dist: f32; inv_dist_cube: f32
     s: f32
-    i: i32; j: i32
     eps_2: f32 = f32(1e-6)
     for i in range(n):
         ax = f32(0.0)
@@ -34,21 +38,28 @@ def lpython_nbody(positions: f32[:, :], weights: f32[:], n: i32) -> f32[n, 2]:
         accelerations[i, 1] = ay
     return accelerations
 
-@ccall
-def init_positions_weights(positions: f32[:, :], weights: f32[:], n: i32):
-    pass
+def check_results(accelerations: f32[:, :], n: i32) -> f32:
+    i: i32
+    l2_norm: f32 = f32(0.0)
+    for i in range(n):
+        assert accelerations[i, 0] == accelerations[i, 1]
+        l2_norm += f32(sqrt(f64(accelerations[i, 0]**f32(2.0) + accelerations[i, 1]**f32(2.0))))
+    return l2_norm
 
 def run_lpython_nbody(n: i32):
     i: i32
     positions: f32[n, 2] = empty((n, 2), dtype=float32)
     weights: f32[n] = empty(n, dtype=float32)
-    accelerations: f32[n] = empty((n, 2), dtype=float32)
-    # replace the following function call with
+    accelerations: f32[n, 2] = empty((n, 2), dtype=float32)
+    # replace the following loop with
     # np.random.RandomState(0).uniform for
     # both positions and weights vectors
-    init_positions_weights(positions, weights, n)
-    accelerations = lpython_nbody(positions, weights, n)
     for i in range(n):
-        print(accelerations[i, 0], accelerations[i, 1])
+        positions[i, 0] = f32(i + 1)
+        positions[i, 1] = f32(i + 2)
+        weights[i] = f32(i + 3)
 
-run_lpython_nbody(10)
+    accelerations = lpython_nbody(positions, weights, n)
+    print(check_results(accelerations, n))
+
+run_lpython_nbody(30000)
--- integration_tests/nbody_util.h
+++
@@ -1,3 +0,0 @@
-#include <inttypes.h>
-
-void init_positions_weights(float* positions, float* weights, int32_t n);
